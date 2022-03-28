34602234a75d58d76e8b27704beaf489c799a782
Leon Teubert <teubert.leon@googlemail.com>
2022-03-28 21:14:50 +0200

    add main class with option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,32 @@
 module Main where
+
 import Git
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Options = Options {
+  path :: String, quiet :: Bool
+}
+
+options :: Parser Options
+options = Options
+      <$> strOption
+          ( long "path"
+         <> short 'p'
+         <> help "Path to the git repository" )
+      <*> switch
+          ( long "quiet"
+         <> short 'q'
+         <> help "Whether to be quiet" )
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = execute =<< execParser opts
+  where
+    opts = info (options <**> helper)
+      ( fullDesc
+     <> progDesc "Parse conventional commit messages of a git repository"
+     <> header "coco.hs" )
 
-
+execute :: Options -> IO ()
+execute (Options path False) = gitLog path >>= mapM_ putStrLn
+execute _ = return ()
